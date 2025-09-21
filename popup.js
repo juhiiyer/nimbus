c5b16f8ee2961a1cbb7c73f5a99cb9cc3b003d3f
diya <didiyaya.2006@gmail.com>
2025-09-21 21:32:03 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById("googleBtn").addEventListener("click", () => {
+  alert("Google Drive connection coming soon!");
+});
+
+document.getElementById("dropboxBtn").addEventListener("click", () => {
+  alert("Dropbox connection coming soon!");
+});
+
+document.getElementById("oneDriveBtn").addEventListener("click", () => {
+  alert("OneDrive connection coming soon!");
+});
